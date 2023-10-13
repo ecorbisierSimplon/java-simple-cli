161e1f5e716dacd9628905c4b9c3ee9b04d52f55
ecorbisierSimplon <ecorbisier.simplon@gmail.com>
2023-10-13 10:57:48 +0200

    User account name

--- Cli.java
+++ Cli.java
@@ -1,17 +1,13 @@
 import java.util.Scanner;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 public class Cli {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in); // Listen to the standard input (console)
 
-		DateTimeFormatter formattedDateNow = DateTimeFormatter.ofPattern("HH:mm:ss.SSSSSSSSS");
-		LocalDateTime today = LocalDateTime.now(); // Add date today
-		String dateNow = today.format(formattedDateNow);
+		String nomSession = System.getProperty("user.name");
 
-		System.out.print(dateNow + " > "); // Prompt
+		System.out.print(nomSession + " > "); // Prompt
 		while (true) { // Infinite loop
 			String command = scanner.nextLine(); // Get input from console as a string
 			String output = ""; // A variable named output of type String
@@ -22,7 +18,7 @@ public class Cli {
 				output = "Command '" + command + "' not found.";
 			}
 			System.out.println(output); // Print with new line (ln)
-			System.out.print(dateNow + " > "); // Prompt
+			System.out.print(nomSession + " > "); // Prompt
 		}
 		scanner.close(); // Best practice, always close a stream when no more needed
 		System.out.println("Bye !");
