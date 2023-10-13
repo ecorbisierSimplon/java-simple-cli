0d143da63b30b90177f03bcd027543c94d1f8fab
ecorbisierSimplon <ecorbisier.simplon@gmail.com>
2023-10-13 09:54:43 +0200

    first init

---
+++ Cli.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Cli {
+
+    public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in); // Listen to the standard input (console)
+		System.out.print("Eric the best> "); // Prompt
+		while (true) { // Infinite loop
+			String command = scanner.nextLine(); // Get input from console as a string
+			String output = ""; // A variable named output of type String
+			if (command.equals("exit")) {
+				break; // Forces exit of the while loop
+			} else {
+				// String concatenation
+				output = "Command '" + command + "' not found.";
+			}
+			System.out.println(output); // Print with new line (ln)
+			System.out.print("> "); // Prompt
+		}
+		scanner.close(); // Best practice, always close a stream when no more needed
+		System.out.println("Bye Youyoun!");
+    }
+
+}
