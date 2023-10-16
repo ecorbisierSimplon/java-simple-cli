93bee760b0f6245aa0fe60a700b82b5be7c1cb9f
ecorbisierSimplon <ecorbisier.simplon@gmail.com>
2023-10-16 12:24:03 +0200

    Correction with teacher's commentary

--- Cli.java
+++ Cli.java
@@ -12,39 +12,56 @@ public class Cli {
 		while (true) { // Infinite loop
 			String command = scanner.nextLine(); // Get input from console as a string
 			String output = ""; // A variable named output of type String
-			if (command.equals("exit")) {
+			if (command.trim().equals("exit")) {
 				break;
-			} else if (command.equals("date")) {
+
+			} else if (command.trim().equals("date")) {
 				LocalDate formattedDateNow = LocalDate.now();
-				System.out.println(formattedDateNow); // Print with new line (ln)
-			} else if (command.equals("time")) {
+				output = formattedDateNow.toString(); // Print the date
+
+			} else if (command.trim().equals("time")) {
 				LocalTime formattedTimeNow = LocalTime.now();
-				System.out.println(formattedTimeNow); // Print with new line (ln)
-			} else if (command.equals("datetime")) {
+				output = formattedTimeNow.toString(); // Print the time
+
+			} else if (command.trim().equals("datetime")) {
 				LocalDateTime formattedDateTimeNow = LocalDateTime.now();
-				System.out.println(formattedDateTimeNow); // Print with new line (ln)
-			} else if (command.equals("useraccount")) {
-				output = System.getProperty("user.name");
-			} else if (command.equals("userhome")) {
-				output = System.getProperty("user.home");
-			} else if (command.equals("os")) {
+				output = formattedDateTimeNow.toString(); // Print the Date and Time
+
+			} else if (command.trim().equals("useraccount")) {
+				output = System.getProperty("user.name"); // Print User name
+
+			} else if (command.trim().equals("userhome")) {
+				output = System.getProperty("user.home"); // Print User Home
+
+			} else if (command.trim().equals("os")) {
 				output = System.getProperty("os.name") + " (" + System.getProperty("os.version") + ")";
-			} else if (command.startsWith("printenv")) {
-				String arguments = command.substring("printenv".length()).trim();
-				String[] argumentArray = arguments.split(" ");
-				output = System.getenv(argumentArray[0]);
-			} else if (command.startsWith("echo ")) {
-				if (!command.equals("echo ")) {
-					output = command.substring("echo ".length()).trim();
+				// Print os'information
+
+			} else if (command.startsWith("printenv ") || command.equals("printenv")) {
+				// This command is used to reject printenv elements whose arguments are not
+				// spaced between printenv and the 1st argument.
+
+				if (!command.trim().equals("printenv")) { // Print value of environnement variable
+					String[] argumentArray = command.split(" ");
+					output = System.getenv(argumentArray[1]);
 				}
-			} else if (command.equals("echo")) {
-				// output = NULL
+				if (output == null) {
+					output = ""; // if environnement variable is null then transform output in argument empty
+				}
+
+			} else if (command.startsWith("echo ") || command.equals("echo")) {
+				// This command is used to reject echo elements whose arguments are not spaced
+				// between echo and the 1st argument.
+				output = !command.trim().equals("echo") ? command.substring("echo ".length()).trim() : "";
+
 			} else {
 				output = "Command '" + command + "' not found.";
 			}
 
-			if (output != "")
-				System.out.println(output); // Print with new line (ln)
+			System.out.println(output); // Print with new line (ln)
+			if (output != "") // If output is not empty
+				System.out.println(); // Adds a blank line for better readability
+
 			System.out.print("> "); // Prompt
 		}
 		// Forces exit of the while loop
