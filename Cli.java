e1813d8fe51ea38ff0091b6184aba0017f6d8c59
ecorbisierSimplon <ecorbisier.simplon@gmail.com>
2023-10-19 14:11:35 +0200

    Brief - A simple Java CLI (part 2) - construct ls environnement V04_b

--- Cli.java
+++ Cli.java
@@ -16,51 +16,56 @@ public class Cli {
 		while (true) { // Infinite loop
 			String command = scanner.nextLine(); // Get input from console as a string
 			String[] commandArray = command.trim().split(" ", 2);
-			String output = ""; // A variable named output of type String
+			// String output = ""; // A variable named output of type String
+			StringBuilder output = new StringBuilder();
+			
 			if (commandArray[0].equals("exit") || commandArray[0].equals("logout")) {
 				break;
 
 			} else if (commandArray[0].equals("date")) {
-				LocalDate formattedDateNow = LocalDate.now();
-				output = formattedDateNow.toString(); // Print the date
+				output.append(LocalDate.now());//formattedDateNow); // Print the date
 
 			} else if (commandArray[0].equals("time")) {
-				LocalTime formattedTimeNow = LocalTime.now();
-				output = formattedTimeNow.toString(); // Print the time
+				output.append(LocalDate.now()); // Print the time
 
 			} else if (commandArray[0].equals("datetime")) {
-				LocalDateTime formattedDateTimeNow = LocalDateTime.now();
-				output = formattedDateTimeNow.toString(); // Print the Date and Time
+				output.append(LocalDateTime.now()); // Print the Date and Time
 
 			} else if (commandArray[0].equals("useraccount")) {
-				output = System.getProperty("user.name"); // Print User name
+				output.append(System.getProperty("user.name")); // Print User name
 
 			} else if (commandArray[0].equals("userhome")) {
-				output = System.getProperty("user.home"); // Print User Home
+				output.append(System.getProperty("user.home")); // Print User Home
 
 			} else if (commandArray[0].equals("os")) {
-				output = System.getProperty("os.name") + " (" + System.getProperty("os.version") + ")";
+				output.append(System.getProperty("os.name"))
+					  .append(" (")
+					  .append(System.getProperty("os.version"))
+					  .append(")");
 				// Print os'information
 
 			} else if (commandArray[0].equals("printenv")) {
 				// This command is used to reject printenv elements whose arguments are not
 				// spaced between printenv and the 1st argument.
 				if (commandArray.length > 1) { // Print value of environnement variable
-					output = System.getenv(commandArray[1]);
-					output = output == null ? "" : output; // if environnement variable is null
+					String test = System.getenv(commandArray[1]);
+					output.append(test == null ? "" : test); // if environnement variable is null
 															// then transform output in argument empty
 				} else { // if not arguments write
 					Map<String, String> varEnv = System.getenv(); // Transforms a string into an array
 																	// with a key and a value linked to the key.
 					for (String key : varEnv.keySet()) { //
-						output += key + "=" + varEnv.get(key) + System.lineSeparator();
+						output.append(key)
+								.append("=")
+								.append(varEnv.get(key))
+								.append(System.lineSeparator());
 					}
 				}
 
 			} else if (commandArray[0].equals("echo") || commandArray[0].equals("print")) {
 				// This command is used to reject echo elements whose arguments are not spaced
 				// between echo and the 1st argument.
-				output = commandArray.length > 1 ? commandArray[1] : "";
+				output.append(commandArray.length > 1 ? commandArray[1] : "");
 
 			} else if (commandArray[0].equals("ls")) {
 
@@ -70,22 +75,22 @@ public class Cli {
 					File dossier = new File(argument);
 					// le "fichier" existe et est un dossier
 					if (dossier.exists() && dossier.isDirectory()) { // Verify if directory existe
-						output = listDirectory(argument); // Create list directory and file
+						output.append(listDirectory(argument)); // Create list directory and file
 
 					}					
 										
 				}
-				if (output.equals(""))
-							output = "Not a directory !";	
+				if (output.length() == 0)
+							output.append("Not a directory !");	
 
 			} else {
-				output = commandArray[0].equals("") ? "Please enter your comand !"
-						: "Command '" + commandArray[0] + "' not found.";
+				output.append(commandArray[0].equals("") ? "Please enter your comand !"
+						: "Command '" + commandArray[0] + "' not found.");
 			}
 
-			System.out.println(output); // Print with new line (ln)
-			if (output != "") // If output is not empty
-				System.out.println(); // Adds a blank line for better readability
+			System.out.println(String.join("", output)); // Print with new line (ln)
+			// if (output != "") // If output is not empty
+				// System.out.println(); // Adds a blank line for better readability
 
 			System.out.print("> "); // Prompt
 		}
@@ -105,8 +110,12 @@ public class Cli {
 			 * sorts the list alphabetically, case insensitive, with the directory first and
 			 * then the files as part 2
 			 */
-			Arrays.sort(liste, Comparator.comparing(File::isDirectory).reversed()
-					.thenComparing((file1, file2) -> file1.getName().compareToIgnoreCase(file2.getName())));
+			Arrays.sort(liste, Comparator.comparing(File::isDirectory)
+					.reversed()
+					.thenComparing((file1, file2) -> file1.getName()
+						.compareToIgnoreCase(file2.getName())
+					)
+			);
 
 			// create a StringBuilder to store sorted files
 			StringBuilder result = new StringBuilder();
